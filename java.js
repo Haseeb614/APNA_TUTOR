5894a08ea774a56c4a673d122b699958e5111e60
Haseeb614 <haseebmallhi45@gmail.com>
2025-12-01 16:09:17 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+// Function to handle the "Registration form" button click
+function Form() {
+    // Define the URL for the Google Form
+    var formUrl = 'https://forms.gle/Ne55AAnqrTPNCJJt9';
+    
+    // Open the URL in a new browser tab/window
+    window.open(formUrl, '_blank');
+}
+
+// Function to go back to the main page (assuming web.html is your main page)
+function ima() {
+     window.location.href ='web.html';
+}
+
+// Function to go to the second page (assuming web2.html is the registration process page)
+function page2() {
+     window.location.href ='web2.html';
+}
+function page3() {
+     window.location.href ='web3.html';
+}
